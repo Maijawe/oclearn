756a07ac6813c9f1657883b69ba3f61236d732db
Thato Bopape <socioryde21@gmail.com>
2025-05-13 11:47:29 +0200

    enabled mongo atlas

--- server/server.js
+++ server/server.js
@@ -28,11 +28,11 @@ app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 
 const upload = multer({ dest: 'uploads/' });
-mongoose.connect('mongodb://127.0.0.1/aiLMSDatabase', { useNewUrlParser: true});
-/* mongoose.connect(process.env.MONGO_URI, {
+//mongoose.connect('mongodb://127.0.0.1/aiLMSDatabase', { useNewUrlParser: true});
+ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}); */
+}); 
 
 
 
